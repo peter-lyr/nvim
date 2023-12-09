794af075150d16c0f3d16626c3258d82eae468b9
peter-lyr <llydrp.ldp@gmail.com>
2023-12-09 22:41:48 +0800

    feat: navarasu/onedark.nvim

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,3 +1,16 @@
 return {
+
+  -- startuptime
   { 'dstein64/vim-startuptime', cmd = 'StartupTime', },
+
+  -- colorscheme
+  {
+    'navarasu/onedark.nvim',
+    lazy = false,
+    priority = 200,
+    config = function()
+      require 'onedark'.load()
+    end,
+  },
+
 }
