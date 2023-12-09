611b73e538a3f97c2e51e4706cfb1eb2d1e69896
peter-lyr <llydrp.ldp@gmail.com>
2023-12-09 22:55:22 +0800

    perf: use catppuccin instead of onedark

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,15 +1,22 @@
 return {
 
   -- startuptime
-  { 'dstein64/vim-startuptime', cmd = 'StartupTime', },
+  {
+    'dstein64/vim-startuptime',
+    cmd = 'StartupTime',
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 
   -- colorscheme
   {
-    'navarasu/onedark.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
     lazy = false,
-    priority = 200,
+    priority = 1000,
     config = function()
-      require 'onedark'.load()
+      vim.cmd.colorscheme 'catppuccin'
     end,
   },
 
