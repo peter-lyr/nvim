62190656f046700d16e6191fecd046e76c3bc6c2
peter-lyr <llydrp.ldp@gmail.com>
2023-12-09 22:24:52 +0800

    refactor:

--- lua/plugins/my.lua
+++ lua/plugins/my.lua
@@ -4,8 +4,8 @@ return {
     dir = '',
     lazy = true,
     keys = {
-      { '<a-h>', function() require 'config.my.window'.width_less() end, mode = { 'n', 'v', }, silent = true, desc = 'my.window: width_less', },
-      { '<a-l>', function() require 'config.my.window'.width_more() end, mode = { 'n', 'v', }, silent = true, desc = 'my.window: width_more', },
+      { '<a-h>', function() require 'config.my.window'.width_less() end,  mode = { 'n', 'v', }, silent = true, desc = 'my.window: width_less', },
+      { '<a-l>', function() require 'config.my.window'.width_more() end,  mode = { 'n', 'v', }, silent = true, desc = 'my.window: width_more', },
       { '<a-k>', function() require 'config.my.window'.height_less() end, mode = { 'n', 'v', }, silent = true, desc = 'my.window: height_less', },
       { '<a-j>', function() require 'config.my.window'.height_more() end, mode = { 'n', 'v', }, silent = true, desc = 'my.window: height_more', },
     },
