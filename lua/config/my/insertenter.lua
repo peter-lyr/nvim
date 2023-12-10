a462bf01521c5817219e8f5562d0ac72bdb64341
peter-lyr <llydrp.ldp@gmail.com>
2023-12-10 17:12:06 +0800

    fix: imap no silent

--- lua/config/my/insertenter.lua
+++ lua/config/my/insertenter.lua
@@ -70,55 +70,55 @@ B.aucmd({ 'BufLeave', 'CmdlineEnter', }, 'my.insertenter: CmdlineEnter', {
 
 B.lazy_map {
   -- reg
-  { '<c-e>',   '<c-r>e',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste <cword>',           silent = true, },
-  { '<c-3>',   '<c-r>3',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste <cWORD>',           silent = true, },
+  { '<c-e>',   '<c-r>e',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste <cword>', },
+  { '<c-3>',   '<c-r>3',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste <cWORD>', },
 
-  { '<c-t>',   '<c-r>=expand("%:t")<cr>',              mode = { 'c', 'i', },      desc = 'my.insertenter: paste %:t',               silent = true, },
-  { '<c-b>',   '<c-r>=bufname()<cr>',                  mode = { 'c', 'i', },      desc = 'my.insertenter: paste bufname()',         silent = true, },
-  { '<c-f>',   '<c-r>=nvim_buf_get_name(0)<cr>',       mode = { 'c', 'i', },      desc = 'my.insertenter: paste nvim_buf_get_name', silent = true, },
-  { '<c-d>',   '<c-r>=getcwd()<cr>',                   mode = { 'c', 'i', },      desc = 'my.insertenter: paste cwd',               silent = true, },
+  { '<c-t>',   '<c-r>=expand("%:t")<cr>',              mode = { 'c', 'i', },      desc = 'my.insertenter: paste %:t', },
+  { '<c-b>',   '<c-r>=bufname()<cr>',                  mode = { 'c', 'i', },      desc = 'my.insertenter: paste bufname()', },
+  { '<c-f>',   '<c-r>=nvim_buf_get_name(0)<cr>',       mode = { 'c', 'i', },      desc = 'my.insertenter: paste nvim_buf_get_name', },
+  { '<c-d>',   '<c-r>=getcwd()<cr>',                   mode = { 'c', 'i', },      desc = 'my.insertenter: paste cwd', },
 
-  { '<c-l>',   '<c-r>=g:curline<cr>',                  mode = { 'c', 'i', },      desc = 'my.insertenter: paste cur line',          silent = true, },
-  { "<c-'>",   '<c-r>=g:single_quote<cr>',             mode = { 'c', 't', },      desc = "my.insertenter: paste in ''",             silent = true, },
-  { "<c-s-'>", '<c-r>=g:double_quote<cr>',             mode = { 'c', 't', },      desc = 'my.insertenter: paste in ""',             silent = true, },
-  { '<c-0>',   '<c-r>=g:parentheses<cr>',              mode = { 'c', 't', },      desc = 'my.insertenter: paste in ()',             silent = true, },
-  { '<c-]>',   '<c-r>=g:bracket<cr>',                  mode = { 'c', 't', },      desc = 'my.insertenter: paste in []',             silent = true, },
-  { '<c-s-]>', '<c-r>=g:brace<cr>',                    mode = { 'c', 't', },      desc = 'my.insertenter: paste in {}',             silent = true, },
-  { '<c-`>',   '<c-r>=g:back_quote<cr>',               mode = { 'c', 't', },      desc = 'my.insertenter: paste in ``',             silent = true, },
-  { '<c-s-.>', '<c-r>=g:angle_bracket<cr>',            mode = { 'c', 't', },      desc = 'my.insertenter: paste in <>',             silent = true, },
+  { '<c-l>',   '<c-r>=g:curline<cr>',                  mode = { 'c', 'i', },      desc = 'my.insertenter: paste cur line', },
+  { "<c-'>",   '<c-r>=g:single_quote<cr>',             mode = { 'c', 't', },      desc = "my.insertenter: paste in ''", },
+  { "<c-s-'>", '<c-r>=g:double_quote<cr>',             mode = { 'c', 't', },      desc = 'my.insertenter: paste in ""', },
+  { '<c-0>',   '<c-r>=g:parentheses<cr>',              mode = { 'c', 't', },      desc = 'my.insertenter: paste in ()', },
+  { '<c-]>',   '<c-r>=g:bracket<cr>',                  mode = { 'c', 't', },      desc = 'my.insertenter: paste in []', },
+  { '<c-s-]>', '<c-r>=g:brace<cr>',                    mode = { 'c', 't', },      desc = 'my.insertenter: paste in {}', },
+  { '<c-`>',   '<c-r>=g:back_quote<cr>',               mode = { 'c', 't', },      desc = 'my.insertenter: paste in ``', },
+  { '<c-s-.>', '<c-r>=g:angle_bracket<cr>',            mode = { 'c', 't', },      desc = 'my.insertenter: paste in <>', },
 
-  { '<c-s>',   '<c-r>"',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste "',                 silent = true, },
-  { '<c-s>',   '<c-\\><c-n>pi',                        mode = { 't', },           desc = 'my.insertenter: paste "',                 silent = true, },
+  { '<c-s>',   '<c-r>"',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste "', },
+  { '<c-s>',   '<c-\\><c-n>pi',                        mode = { 't', },           desc = 'my.insertenter: paste "', },
 
-  { '<c-v>',   '<c-r>+',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste +',                 silent = true, },
-  { '<c-v>',   '<c-\\><c-n>"+pi',                      mode = { 't', },           desc = 'my.insertenter: paste +',                 silent = true, },
+  { '<c-v>',   '<c-r>+',                               mode = { 'c', 'i', },      desc = 'my.insertenter: paste +', },
+  { '<c-v>',   '<c-\\><c-n>"+pi',                      mode = { 't', },           desc = 'my.insertenter: paste +', },
 
   -- cursor
   { 'k',       "(v:count == 0 && &wrap) ? 'gk' : 'k'", mode = { 'n', 'v', },      desc = 'my.insertenter: k',                       silent = true, expr = true, },
   { 'j',       "(v:count == 0 && &wrap) ? 'gj' : 'j'", mode = { 'n', 'v', },      desc = 'my.insertenter: j',                       silent = true, expr = true, },
 
-  { '<a-k>',   '<UP>',                                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: up',                      silent = true, },
-  { '<a-j>',   '<DOWN>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: down',                    silent = true, },
+  { '<a-k>',   '<UP>',                                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: up', },
+  { '<a-j>',   '<DOWN>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: down', },
 
-  { '<a-s-k>', '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up',                    silent = true, },
-  { '<a-s-j>', '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down',                  silent = true, },
-  { '<c-k>',   '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up',                    silent = true, },
-  { '<c-j>',   '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down',                  silent = true, },
-  { '<c-s-k>', '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up',                    silent = true, },
-  { '<c-s-j>', '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down',                  silent = true, },
+  { '<a-s-k>', '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up', },
+  { '<a-s-j>', '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down', },
+  { '<c-k>',   '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up', },
+  { '<c-j>',   '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down', },
+  { '<c-s-k>', '<UP><UP><UP><UP><UP>',                 mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 up', },
+  { '<c-s-j>', '<DOWN><DOWN><DOWN><DOWN><DOWN>',       mode = { 't', 'c', 'i', }, desc = 'my.insertenter: 5 down', },
 
-  { '<a-i>',   '<HOME>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: home',                    silent = true, },
-  { '<a-s-i>', '<HOME>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: home',                    silent = true, },
-  { '<a-o>',   '<END>',                                mode = { 't', 'c', 'i', }, desc = 'my.insertenter: end',                     silent = true, },
-  { '<a-s-o>', '<END>',                                mode = { 't', 'c', 'i', }, desc = 'my.insertenter: end',                     silent = true, },
+  { '<a-i>',   '<HOME>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: home', },
+  { '<a-s-i>', '<HOME>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: home', },
+  { '<a-o>',   '<END>',                                mode = { 't', 'c', 'i', }, desc = 'my.insertenter: end', },
+  { '<a-s-o>', '<END>',                                mode = { 't', 'c', 'i', }, desc = 'my.insertenter: end', },
 
-  { '<a-l>',   '<RIGHT>',                              mode = { 't', 'c', 'i', }, desc = 'my.insertenter: right',                   silent = true, },
-  { '<a-h>',   '<LEFT>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: left',                    silent = true, },
-  { '<a-s-l>', '<c-RIGHT>',                            mode = { 't', 'c', 'i', }, desc = 'my.insertenter: ctrl right',              silent = true, },
-  { '<a-s-h>', '<c-LEFT>',                             mode = { 't', 'c', 'i', }, desc = 'my.insertenter: ctrl left',               silent = true, },
+  { '<a-l>',   '<RIGHT>',                              mode = { 't', 'c', 'i', }, desc = 'my.insertenter: right', },
+  { '<a-h>',   '<LEFT>',                               mode = { 't', 'c', 'i', }, desc = 'my.insertenter: left', },
+  { '<a-s-l>', '<c-RIGHT>',                            mode = { 't', 'c', 'i', }, desc = 'my.insertenter: ctrl right', },
+  { '<a-s-h>', '<c-LEFT>',                             mode = { 't', 'c', 'i', }, desc = 'my.insertenter: ctrl left', },
 
   { '<c-f11>', 'gf',                                   mode = { 'n', 'v', },      desc = 'my.insertenter: gf',                      silent = true, },
 
   -- esc
-  { '<esc>',   '<c-\\><c-n>',                          mode = { 't', },           desc = 'my.insertenter: esc',                     silent = true, },
+  { '<esc>',   '<c-\\><c-n>',                          mode = { 't', },           desc = 'my.insertenter: esc', },
 }
