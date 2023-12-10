f7865afa501786aefc5486610cf6fef37e57e9fd
peter-lyr <llydrp.ldp@gmail.com>
2023-12-11 01:16:06 +0800

    fix: commemt

--- lua/config/test/comment.lua
+++ lua/config/test/comment.lua
@@ -1,6 +1,36 @@
 local B = require 'base'
 
 -- nerdcommenter
+vim.g.NERDSpaceDelims = 1
+vim.g.NERDDefaultAlign = 'left'
+vim.g.NERDCommentEmptyLines = 1
+vim.g.NERDTrimTrailingWhitespace = 1
+vim.g.NERDToggleCheckAllLines = 1
+vim.g.NERDCustomDelimiters = {
+  markdown = {
+    left = '<!--',
+    right = '-->',
+    leftAlt = '[',
+    rightAlt = ']: #',
+  },
+  c = {
+    left = '//',
+    right = '',
+    leftAlt = '/*',
+    rightAlt = '*/',
+  },
+}
+
+B.aucmd({ 'BufEnter', }, 'test.commemt.BufEnter', {
+  callback = function()
+    if vim.bo.ft == 'python' then
+      vim.g.NERDSpaceDelims = 0
+    else
+      vim.g.NERDSpaceDelims = 1
+    end
+  end,
+})
+
 B.lazy_map {
   { '<leader>co', "}kvip:call nerdcommenter#Comment('x', 'invert')<CR>", mode = { 'n', }, desc = 'Nerdcommenter invert a paragraph', },
   { '<leader>cp', "}kvip:call nerdcommenter#Comment('x', 'toggle')<CR>", mode = { 'n', }, desc = 'Nerdcommenter toggle a paragraph', },
