8e375fcf13d7daa8f02142114fd76af70f05dbeb
peter-lyr <llydrp.ldp@gmail.com>
2023-12-11 10:07:26 +0800

    fix: colorscheme 1000ms do

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -17,7 +17,9 @@ return {
     -- lazy = false,
     -- priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin'
+      vim.fn.timer_start(1000, function()
+        vim.cmd.colorscheme 'catppuccin'
+      end)
     end,
   },
 
