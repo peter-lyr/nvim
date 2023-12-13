425d8389cb7ab90afa2363d3fbe681107f090753
peter-lyr <llydrp.ldp@gmail.com>
2023-12-14 01:27:49 +0800

    M._prepare_buffer()

--- lua/config/my/args.lua
+++ lua/config/my/args.lua
@@ -35,6 +35,57 @@ function M.start_bcomp_last()
   B.system_run('start silent', 'bcomp "%s" "%s"', M.file1, M.file2)
 end
 
+----------------------
+-- multiple files
+----------------------
+M.files = {}
+
+function M._stack_files_from_qflist()
+  M.files = {}
+  for _, v in ipairs(vim.fn.getqflist()) do
+    local file = vim.api.nvim_buf_get_name(v['bufnr'])
+    if vim.tbl_contains(M.files, file) == false then
+      M.files[#M.files + 1] = file
+    end
+  end
+end
+
+function M._ro_buffer(lines)
+  vim.cmd 'new'
+  vim.cmd 'set noro'
+  vim.cmd 'set ma'
+  vim.cmd 'set ft=c'
+  vim.fn.setline(1, lines)
+  vim.cmd 'diffthis'
+  vim.cmd 'set ro'
+  vim.cmd 'set noma'
+end
+
+function M._ma_buffer(lines)
+  vim.cmd 'vnew'
+  vim.cmd 'set noro'
+  vim.cmd 'set ma'
+  vim.fn.setline(1, lines)
+  vim.cmd 'set ft=c'
+  vim.cmd 'diffthis'
+  vim.keymap.set('n', 'dd', '0D')
+  vim.cmd 'call feedkeys("zR$")'
+end
+
+function M._prepare_buffer()
+  M._ro_buffer(M.files)
+  M._ma_buffer(M.files)
+end
+
+function M.rename_files()
+  M._stack_files_from_qflist()
+  if #M.files == 0 then
+    B.notify_info 'no files in qflist'
+    return
+  end
+  M._prepare_buffer()
+end
+
 B.create_user_command_with_M(M, 'Args')
 
 return M
