79934ee2bcb849ec88cad32262227d7c42228bee
peter-lyr <llydrp.ldp@gmail.com>
2023-12-13 23:06:51 +0800

    del statuscol.lua

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -50,7 +50,24 @@ return {
     'luukvbaal/statuscol.nvim',
     event = { 'BufReadPost', 'BufNewFile', },
     config = function()
-      require 'config.test.statuscol'.setup()
+      local builtin = require 'statuscol.builtin'
+      require 'statuscol'.setup {
+        bt_ignore = { 'terminal', 'nofile', },
+        relculright = true,
+        segments = {
+          { text = { builtin.foldfunc, }, click = 'v:lua.ScFa', },
+          {
+            sign = { name = { 'Diagnostic', }, maxwidth = 2, colwidth = 1, auto = true, },
+            click = 'v:lua.ScSa',
+          },
+          { text = { builtin.lnumfunc, }, click = 'v:lua.ScLa', },
+          {
+            sign = { name = { '.*', }, namespace = { '.*', }, maxwidth = 2, colwidth = 1, wrap = true, auto = true, },
+            click = 'v:lua.ScSa',
+          },
+          { text = { '│', }, condition = { builtin.not_empty, }, },
+        },
+      }
     end,
   },
 
