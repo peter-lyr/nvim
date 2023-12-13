a42a589db2054f03960f03f4eb57e1f2b92deba2
peter-lyr <llydrp.ldp@gmail.com>
2023-12-13 12:07:24 +0800

    del whichkey.lua

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -32,7 +32,16 @@ return {
       vim.o.timeoutlen = 300
     end,
     config = function()
-      require 'config.test.whichkey'.setup()
+      require 'which-key'.setup {
+        window = {
+          border = 'single',
+          winblend = 12,
+        },
+        layout = {
+          height = { min = 4, max = 80, },
+          width = { min = 20, max = 200, },
+        },
+      }
     end,
   },
 
