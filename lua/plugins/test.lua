97ad555096e275872f485da78339cc42fae497ff
peter-lyr <llydrp.ldp@gmail.com>
2023-12-13 23:10:23 +0800

    del comment.lua

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -91,8 +91,42 @@ return {
     dependencies = {
       'preservim/nerdcommenter',
     },
+    keys = {
+      { '<leader>co', "}kvip:call nerdcommenter#Comment('x', 'invert')<CR>", mode = { 'n', }, desc = 'Nerdcommenter invert a paragraph', },
+      { '<leader>cp', "}kvip:call nerdcommenter#Comment('x', 'toggle')<CR>", mode = { 'n', }, desc = 'Nerdcommenter toggle a paragraph', },
+    },
     config = function()
-      require 'config.test.comment'
+      -- nerdcommenter
+      vim.g.NERDSpaceDelims = 1
+      vim.g.NERDDefaultAlign = 'left'
+      vim.g.NERDCommentEmptyLines = 1
+      vim.g.NERDTrimTrailingWhitespace = 1
+      vim.g.NERDToggleCheckAllLines = 1
+      vim.g.NERDCustomDelimiters = {
+        markdown = {
+          left = '<!--',
+          right = '-->',
+          leftAlt = '[',
+          rightAlt = ']: #',
+        },
+        c = {
+          left = '//',
+          right = '',
+          leftAlt = '/*',
+          rightAlt = '*/',
+        },
+      }
+      require 'base'.aucmd({ 'BufEnter', }, 'test.commemt.BufEnter', {
+        callback = function()
+          if vim.bo.ft == 'python' then
+            vim.g.NERDSpaceDelims = 0
+          else
+            vim.g.NERDSpaceDelims = 1
+          end
+        end,
+      })
+      -- Comment.nvim
+      require 'Comment'.setup {}
     end,
   },
 
