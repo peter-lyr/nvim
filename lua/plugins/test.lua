28e6c4c9651ebb441420fbca48be8fab5be605de
peter-lyr <llydrp.ldp@gmail.com>
2023-12-13 12:46:20 +0800

    .

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -142,6 +142,7 @@ return {
     end,
   },
 
+  -- notify
   {
     'rcarriga/nvim-notify',
     keys = {
@@ -151,12 +152,8 @@ return {
       require 'notify'.setup {
         top_down = false,
         timeout = 3000,
-        max_height = function()
-          return math.floor(vim.o.lines * 0.75)
-        end,
-        max_width = function()
-          return math.floor(vim.o.columns * 0.75)
-        end,
+        max_height = function() return math.floor(vim.o.lines * 0.75) end,
+        max_width = function() return math.floor(vim.o.columns * 0.75) end,
       }
       vim.notify = require 'notify'
     end,
