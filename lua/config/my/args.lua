c2cb113aaeaf7007bc03cbcb86eb6d3b236c14d0
peter-lyr <llydrp.ldp@gmail.com>
2023-12-14 00:52:28 +0800

    two files

--- lua/config/my/args.lua
+++ lua/config/my/args.lua
@@ -4,50 +4,35 @@ local B = require 'base'
 
 M.lua = B.getlua(debug.getinfo(1)['source'])
 
-M.args = {
-  ['1'] = '',
-  ['2'] = '',
-}
+----------------------
+-- two files
+----------------------
+M.file1 = ''
+M.file2 = ''
 
-function M.put_1_cur_file(file)
-  if not file then
-    M.args['1'] = vim.api.nvim_buf_get_name(0)
-  end
-end
+function M.stack_file1() M.file1 = vim.api.nvim_buf_get_name(0) end
 
-function M.put_2_cur_file(file)
-  if not file then
-    M.args['2'] = vim.api.nvim_buf_get_name(0)
-  end
-end
-
-function M.print_args()
-  local info = ''
-  for k, v in pairs(M.args) do
-    info = info .. string.format('%d: %s\n', k, v)
-  end
-  B.notify_info(info)
-end
+function M.stack_file2() M.file2 = vim.api.nvim_buf_get_name(0) end
 
 function M.start_bcomp_cur()
-  M.put_2_cur_file()
-  if #M.args['1'] > 0 then
-    B.system_run('start silent', 'bcomp "%s" "%s"', M.args['1'], M.args['2'])
+  M.stack_file2()
+  if #M.file1 > 0 then
+    B.system_run('start silent', 'bcomp "%s" "%s"', M.file1, M.file2)
   else
-    B.notify_info 'Please put_1_cur_file first.'
+    B.notify_info 'Please stack_file1 first.'
   end
 end
 
 function M.start_bcomp_last()
-  if #M.args['1'] == 0 then
-    B.notify_info 'Please put_1_cur_file first.'
+  if #M.file1 == 0 then
+    B.notify_info 'Please stack_file1 first.'
     return
   end
-  if #M.args['2'] == 0 then
-    B.notify_info 'Please put_2_cur_file first.'
+  if #M.file2 == 0 then
+    B.notify_info 'Please stack_file2 first.'
     return
   end
-  B.system_run('start silent', 'bcomp "%s" "%s"', M.args['1'], M.args['2'])
+  B.system_run('start silent', 'bcomp "%s" "%s"', M.file1, M.file2)
 end
 
 B.create_user_command_with_M(M, 'Args')
