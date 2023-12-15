bccd6013f3c838903184add26a8b7a48f4a09fd6
peter-lyr <llydrp.ldp@gmail.com>
2023-12-16 02:15:46 +0800

    M.disable_doc_must_system_open()

--- lua/config/my/drag.lua
+++ lua/config/my/drag.lua
@@ -11,17 +11,35 @@ M.DOC_EXTS = {
   'pdf',
 }
 
-M._en = 1
+M._en_total = 1
+M._en_bin_must_xxd = 1
+M._en_doc_must_system_open = 1
 M._last_file = ''
 M._callbacks = {}
 
 -- eanblea or disable
 function M.enable()
-  M._en = 1
+  M._en_total = 1
 end
 
 function M.disable()
-  M._en = nil
+  M._en_total = nil
+end
+
+function M.enable_bin_must_xxd()
+  M._en_bin_must_xxd = 1
+end
+
+function M.disable_bin_must_xxd()
+  M._en_bin_must_xxd = nil
+end
+
+function M.enable_doc_must_system_open()
+  M._en_doc_must_system_open = 1
+end
+
+function M.disable_doc_must_system_open()
+  M._en_doc_must_system_open = nil
 end
 
 -- common
@@ -56,7 +74,6 @@ function M._check_bin(cur_file)
   if not cur_file then cur_file = vim.api.nvim_buf_get_name(0) end
   if M._is_bin(cur_file) then
     return {
-      ['bin xxd'] = function() M.bin_xxd(cur_file) end,
       ['bin xxd and delete buffer'] = function()
         M.bin_xxd(cur_file)
         M._delete_buffer()
@@ -73,8 +90,7 @@ end
 function M._check_doc(cur_file)
   if not cur_file then cur_file = vim.api.nvim_buf_get_name(0) end
   if B.is_file_in_extensions(M.DOC_EXTS, cur_file) then
-    local tbl = {
-      ['system open'] = function() M.system_open(cur_file) end,
+    return {
       ['bin xxd and delete buffer'] = function()
         M.bin_xxd(cur_file)
         M._delete_buffer()
@@ -84,14 +100,13 @@ function M._check_doc(cur_file)
         M._delete_buffer()
       end,
     }
-    return tbl
   end
 end
 
 -- aucmd
 B.aucmd('BufReadPre', 'my.drag.BufReadPre', {
   callback = function(ev)
-    if M._en then
+    if M._en_total then
       local file = B.get_full_name(ev.file)
       M._callbacks = { ['Nop'] = function() end, }
       M._callbacks = B.merge_dict(M._callbacks, M._check_bin(file))
@@ -101,8 +116,17 @@ B.aucmd('BufReadPre', 'my.drag.BufReadPre', {
 })
 
 B.aucmd('BufReadPost', 'my.drag.BufReadPost', {
-  callback = function()
-    if M._en then
+  callback = function(ev)
+    if M._en_total then
+      if M._en_doc_must_system_open and B.is_file_in_extensions(M.DOC_EXTS, ev.file) then
+        M.system_open(ev.file)
+        M._delete_buffer()
+        return
+      elseif M._en_bin_must_xxd and M._is_bin(ev.file) then
+        M.bin_xxd(ev.file)
+        M._delete_buffer()
+        return
+      end
       local count = B.get_dict_count(M._callbacks)
       if count == 1 then
         for _, callback in pairs(M._callbacks) do callback() end
@@ -120,7 +144,7 @@ B.aucmd('BufReadPost', 'my.drag.BufReadPost', {
 
 B.aucmd('BufEnter', 'my.drag.BufEnter', {
   callback = function(ev)
-    if M._en then
+    if M._en_total then
       M._last_file = B.get_full_name(ev.file)
     end
   end,
