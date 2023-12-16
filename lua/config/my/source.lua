b87481834676be1bfabde6040a535083eb9d4e2e
peter-lyr <llydrp.ldp@gmail.com>
2023-12-16 23:06:55 +0800

    .

--- lua/config/my/source.lua
+++ lua/config/my/source.lua
@@ -2,15 +2,11 @@ local M = {}
 
 local B = require 'base'
 
-M.source = B.getsource(debug.getinfo(1)['source'])
-M.lua = B.getlua(M.source)
-
 function M.main(file)
   if not file then
     file = vim.api.nvim_buf_get_name(0)
   end
-  local lua = B.getlua(file)
-  package.loaded[lua] = nil
+  package.loaded[B.getlua(file)] = nil
   B.cmd('source %s', file)
 end
 
