fff452c62037dd948dc3a71c17a59be6d5d22f6a
peter-lyr <llydrp.ldp@gmail.com>
2023-12-17 13:47:10 +0800

    <cmd>WhichKey q<cr>

--- lua/config/my/nmaps.lua
+++ lua/config/my/nmaps.lua
@@ -12,31 +12,31 @@ end
 
 lazy_map {
   -- cmdline
-  { '<leader>;',           ':',               mode = { 'n', 'v', }, silent = false, desc = 'my.maps: cmdline', },
+  { '<leader>;',           ':',                   mode = { 'n', 'v', }, silent = false, desc = 'my.maps: cmdline', },
   -- record
-  { 'q',                   '<nop>',           mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: <nop>', },
-  { 'Q',                   'q',               mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: q', },
+  { 'q',                   '<cmd>WhichKey q<cr>', mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: <nop>', },
+  { 'Q',                   'q',                   mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: q', },
   -- cd
-  { 'c.',                  '<cmd>cd %:h<cr>', mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: cd %:h', },
-  { 'cu',                  '<cmd>cd ..<cr>',  mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: cd ..', },
+  { 'c.',                  '<cmd>cd %:h<cr>',     mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: cd %:h', },
+  { 'cu',                  '<cmd>cd ..<cr>',      mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: cd ..', },
   -- undo
-  { 'U',                   '<c-r>',           mode = { 'n', },      silent = true,  desc = 'my.maps: redo', },
+  { 'U',                   '<c-r>',               mode = { 'n', },      silent = true,  desc = 'my.maps: redo', },
   -- scroll horizontally
-  { '<S-ScrollWheelDown>', '10zl',            mode = { 'n', 'v', }, silent = false, desc = 'my.maps: scroll right horizontally', },
-  { '<S-ScrollWheelUp>',   '10zh',            mode = { 'n', 'v', }, silent = false, desc = 'my.maps: scroll left horizontally', },
+  { '<S-ScrollWheelDown>', '10zl',                mode = { 'n', 'v', }, silent = false, desc = 'my.maps: scroll right horizontally', },
+  { '<S-ScrollWheelUp>',   '10zh',                mode = { 'n', 'v', }, silent = false, desc = 'my.maps: scroll left horizontally', },
   -- e!
-  { 'qq',                  '<cmd>e!<cr>',     mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: e!', },
+  { 'qq',                  '<cmd>e!<cr>',         mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: e!', },
   -- cursor
-  { '<c-j>',               '5j',              mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: 5j', },
-  { '<c-k>',               '5k',              mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: 5k', },
+  { '<c-j>',               '5j',                  mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: 5j', },
+  { '<c-k>',               '5k',                  mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: 5k', },
   -- copy_paste
-  { '<a-y>',               '"+y',             mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+y', },
-  { '<a-d>',               '"+d',             mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+d', },
-  { '<a-c>',               '"+c',             mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+c', },
-  { '<a-p>',               '"+p',             mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+p', },
-  { '<a-s-p>',             '"+P',             mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+P', },
+  { '<a-y>',               '"+y',                 mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+y', },
+  { '<a-d>',               '"+d',                 mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+d', },
+  { '<a-c>',               '"+c',                 mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+c', },
+  { '<a-p>',               '"+p',                 mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+p', },
+  { '<a-s-p>',             '"+P',                 mode = { 'n', 'v', }, silent = true,  desc = 'my.maps: "+P', },
   -- for youdao dict
-  { '<c-c>',               '"+y',             mode = { 'v', },      silent = true,  desc = 'my.maps: "+y', },
+  { '<c-c>',               '"+y',                 mode = { 'v', },      silent = true,  desc = 'my.maps: "+y', },
 }
 
 lazy_map {
