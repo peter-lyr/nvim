50c0048e8c42919fe9b81e8f21819f0517576a30
peter-lyr <llydrp.ldp@gmail.com>
2023-12-28 19:24:54 +0800

    my.toggle: signcolumn

--- lua/config/my/toggle.lua
+++ lua/config/my/toggle.lua
@@ -32,7 +32,7 @@ M.renu = function()
 end
 
 M.signcolumn = function()
-  if vim.o.signcolumn == true then
+  if vim.o.signcolumn == 'no' then
     vim.cmd 'set signcolumn=auto:1'
   else
     vim.cmd 'set signcolumn=no'
@@ -67,12 +67,12 @@ B.del_map({ 'n', 'v', }, '<leader>t')
 require 'which-key'.register { ['<leader>t'] = { name = 'my.toggle', }, }
 
 B.lazy_map {
-  { '<leader>td', function() require 'config.my.toggle'.diff() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: diff', },
-  { '<leader>tw', function() require 'config.my.toggle'.wrap() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: wrap', },
-  { '<leader>tr', function() require 'config.my.toggle'.renu() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: renu', },
-  { '<leader>ts', function() require 'config.my.toggle'.signcolumn() end,   mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: signcolumn', },
-  { '<leader>tc', function() require 'config.my.toggle'.conceallevel() end, mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: conceallevel', },
-  { '<leader>tk', function() require 'config.my.toggle'.iskeyword() end,    mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: iskeyword', },
+  { '<leader>td', function() M.diff() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: diff', },
+  { '<leader>tw', function() M.wrap() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: wrap', },
+  { '<leader>tr', function() M.renu() end,         mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: renu', },
+  { '<leader>ts', function() M.signcolumn() end,   mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: signcolumn', },
+  { '<leader>tc', function() M.conceallevel() end, mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: conceallevel', },
+  { '<leader>tk', function() M.iskeyword() end,    mode = { 'n', 'v', }, silent = true, desc = 'my.toggle: iskeyword', },
 }
 
 
