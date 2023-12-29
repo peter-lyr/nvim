e98c72ac2b1a4c2dc8aa2287151b9615c512a663
peter-lyr <llydrp.ldp@gmail.com>
2023-12-29 16:32:31 +0800

    mkdp_highlight.css

--- lua/config/my/markdown.lua
+++ lua/config/my/markdown.lua
@@ -1,5 +1,10 @@
 local M = {}
 
--- local B = require 'base'
+local B = require 'base'
+
+M.source = B.getsource(debug.getinfo(1)['source'])
+M.lua = B.getlua(M.source)
+
+vim.g.mkdp_highlight_css = B.get_filepath(B.getcreate_dir(M.source .. '.css'), 'mkdp_highlight.css').filename
 
 return M
