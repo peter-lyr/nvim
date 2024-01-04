b646e4930356c7221dbd3303a6b7651fee66720a
peter-lyr <llydrp.ldp@gmail.com>
2024-01-04 23:37:16 +0800

    my.markdown.BufReadPre

--- lua/config/my/markdown.lua
+++ lua/config/my/markdown.lua
@@ -59,4 +59,26 @@ function M.make_url()
   end
 end
 
+B.aucmd({ 'BufReadPre', }, 'my.markdown.BufReadPre', {
+  pattern = { '*.md', },
+  callback = function(ev)
+    if vim.fn.getfsize(vim.api.nvim_buf_get_name(ev.buf)) == 0 then
+      B.set_timeout(10, function()
+        vim.cmd 'norm ggdG'
+        vim.fn.setline(1, {
+          string.format('Copyright (c) %s %s. All Rights Reserved.', vim.fn.strftime '%Y', 'liudepei'),
+          vim.fn.strftime '%Y%m%d-%A-%H%M%S',
+        })
+        vim.cmd 'norm vip cs'
+        vim.fn.setline('$', {
+          '<!-- toc -->',
+          '# hi',
+        })
+        vim.lsp.buf.format()
+        vim.cmd 'norm Gw'
+      end)
+    end
+  end,
+})
+
 return M
