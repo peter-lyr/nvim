7d7be15a90533e314e6c08647f88ce9a0cc19224
peter-lyr <llydrp.ldp@gmail.com>
2024-01-18 17:46:23 +0800

    del dos2unix.exe

--- .home/.ssh/dos2unix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Copyright (c) 2024 liudepei. All Rights Reserved.
-REM create at 2024/01/18 11:40:00 Thursday
-
-REM  cd /d %userprofile%\\.ssh
-REM  dos2unix.exe id_* config
-REM  ssh -T git@github.com
