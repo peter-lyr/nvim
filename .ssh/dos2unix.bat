ad06fbb1c95bb43e5bb407d69fbe8b777b21fe69
peter-lyr <llydrp.ldp@gmail.com>
2024-01-18 11:57:42 +0800

    ssh -T git@github.com

--- .ssh/dos2unix.bat
+++ .ssh/dos2unix.bat
@@ -2,6 +2,10 @@
 REM Copyright (c) 2024 liudepei. All Rights Reserved.
 REM create at 2024/01/18 11:40:00 Thursday
 
-cd %~dp0
+set dp=%~dp0
+cd /d %userprofile%
+xcopy %dp%\\ %.ssh\\ /s /e /f /y
 dos2unix.exe id_* config
+ssh -T git@github.com
+
 pause
