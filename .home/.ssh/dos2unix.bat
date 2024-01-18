4b1d7f6a432cd598f3cfaf25c99eb36a52c78344
peter-lyr <llydrp.ldp@gmail.com>
2024-01-18 17:44:59 +0800

    do not dos2unix.exe

--- .home/.ssh/dos2unix.bat
+++ .home/.ssh/dos2unix.bat
@@ -2,6 +2,6 @@
 REM Copyright (c) 2024 liudepei. All Rights Reserved.
 REM create at 2024/01/18 11:40:00 Thursday
 
-cd /d %userprofile%\\.ssh
-dos2unix.exe id_* config
-ssh -T git@github.com
+REM  cd /d %userprofile%\\.ssh
+REM  dos2unix.exe id_* config
+REM  ssh -T git@github.com
