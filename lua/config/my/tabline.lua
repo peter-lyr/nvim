eae2e246e5754e463142e3b4b97c96fedb4feb2b
peter-lyr <llydrp.ldp@gmail.com>
2024-02-06 10:45:05 +0800

    guicursor blink

--- lua/config/my/tabline.lua
+++ lua/config/my/tabline.lua
@@ -286,7 +286,7 @@ vim.cmd [[
   hi tblsel guifg=#f8dfcf guibg=#777777 gui=bold
   hi tbltab guifg=#64e66e guibg=NONE gui=bold
   hi tblfil guifg=gray
-  hi NormalNC guibg=#1a1816 guifg=gray
+  set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175
   hi CursorColumn guibg=#243548
 ]]
 
