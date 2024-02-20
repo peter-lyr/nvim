ece733886d00e97656cbab4e188795db34c779ad
peter-lyr <llydrp.ldp@gmail.com>
2024-02-20 15:09:07 +0800

    start ''

--- lua/config/my/nmaps.lua
+++ lua/config/my/nmaps.lua
@@ -45,8 +45,8 @@ lazy_map {
 }
 
 lazy_map {
-  { 'q.', function() vim.cmd 'silent !explorer %:p:h' end,               mode = { 'n', 'v', }, silent = true, desc = 'my.maps: explorer %:h', },
-  { 'qw', function() vim.cmd('silent !explorer ' .. vim.loop.cwd()) end, mode = { 'n', 'v', }, silent = true, desc = 'my.maps: explorer cwd', },
+  { 'q.', function() vim.cmd 'silent !start "" %:p:h' end,               mode = { 'n', 'v', }, silent = true, desc = 'my.maps: explorer %:h', },
+  { 'qw', function() vim.cmd('silent !start "" ' .. vim.loop.cwd()) end, mode = { 'n', 'v', }, silent = true, desc = 'my.maps: explorer cwd', },
   { 'qs', function() vim.cmd 'silent !cmd /c start "" %:p' end,          mode = { 'n', 'v', }, silent = true, desc = 'my.maps: start %:h', },
 }
 
