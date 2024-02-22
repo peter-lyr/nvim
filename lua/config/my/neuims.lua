2ef54b2ad9dca3a9cb59fdeb24eebcb464e23ece
peter-lyr <llydrp.ldp@gmail.com>
2024-02-22 16:22:12 +0800

    neuims_txt

--- lua/config/my/neuims.lua
+++ lua/config/my/neuims.lua
@@ -8,6 +8,7 @@ local B = require 'base'
 function M.change_language(lang)
   vim.g.lang = lang
   vim.g.res = 1
+  vim.g.neuims_txt = B.depei .. '\\neuims.txt'
   vim.cmd [[
     python << EOF
 import win32api
@@ -20,11 +21,12 @@ LANG = {
 try:
   hwnd = win32gui.GetForegroundWindow()
   language = LANG[vim.eval('g:lang')]
+  neuims_txt = vim.eval('g:neuims_txt')
   result = win32api.SendMessage(hwnd, WM_INPUTLANGCHANGEREQUEST, 0, language)
   vim.command(f'let g:res = {result}')
-  # import time
-  # with open(r'D:\Desktop\neuims.txt', 'ab') as f:
-  #   f.write((f"{vim.eval('g:lang')}: {time.time()}\n").encode('utf-8'))
+  import time
+  with open(neuims_txt, 'ab') as f:
+    f.write((f"{vim.eval('g:lang')}: {time.time()}\n").encode('utf-8'))
 except Exception as e:
   print('change_language - Exception:', e)
 EOF
