1acecf1e5d2ed6555da64f0031c681ad3e979a7b
peter-lyr <llydrp.ldp@gmail.com>
2024-02-29 17:54:24 +0800

    sessions absolute = nil

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -288,7 +288,7 @@ return {
       require 'sessions'.setup {
         events = { 'VimLeavePre', },
         session_filepath = vim.fn.stdpath 'data' .. '\\sessions',
-        absolute = true,
+        absolute = nil,
       }
     end,
   },
