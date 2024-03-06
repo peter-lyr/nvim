cb0c31f839fda6745cd8ac72e557fce25932764b
peter-lyr <llydrp.ldp@gmail.com>
2024-03-06 21:19:47 +0800

    .

--- lua/config/my/tabline.lua
+++ lua/config/my/tabline.lua
@@ -231,6 +231,7 @@ function M.restore_hidden_stack_main()
       vim.cmd 'wincmd H'
       vim.cmd 'wincmd w'
       vim.cmd 'wincmd H'
+      vim.api.nvim_win_set_width(0, require 'nvim-tree.view'.View.width)
       vim.cmd 'wincmd w'
     else
       vim.cmd 'wincmd H'
