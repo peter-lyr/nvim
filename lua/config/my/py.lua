348f4461162b11f26499246e0e2420a993b02cf0
peter-lyr <llydrp.ldp@gmail.com>
2024-03-09 20:19:15 +0800

    tail

--- lua/config/my/py.lua
+++ lua/config/my/py.lua
@@ -16,7 +16,7 @@ function M.cmdline_enter(py_file)
   local tail = vim.fn.fnamemodify(py_file, ':t')
   local args = vim.fn.input(string.format('python %s ', tail))
   B.histadd_en = 1
-  B.system_run('start', '%s && python %s %s', B.system_cd(head), py_file, args)
+  B.system_run('start', '%s && python %s %s', B.system_cd(head), tail, args)
 end
 
 function M.sel_run_py()
