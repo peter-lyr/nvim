cfc6f0ebe9c3532e7fc03f325f95d73c73b30f82
peter-lyr <llydrp.ldp@gmail.com>
2024-03-13 19:22:46 +0800

    M.bdelete_ex_cur_root()

--- lua/config/my/window.lua
+++ lua/config/my/window.lua
@@ -200,7 +200,22 @@ end
 function M.bdelete_ex_cur_root()
   local cwd = B.rep_backslash_lower(vim.fn['ProjectRootGet'](B.buf_get_name_0()))
   local cur_root = require 'config.nvim.telescope'.cur_root[cwd]
-  print("cur_root:", cur_root)
+  if cur_root and cur_root ~= cwd then
+    local curbuf = vim.fn.bufnr()
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+      local fname = B.rep_backslash_lower(vim.api.nvim_buf_get_name(bufnr))
+      if cwd == B.rep_backslash_lower(vim.fn['ProjectRootGet'](fname)) then
+        if string.sub(fname, 1, #cur_root) ~= cur_root or string.sub(fname, #cur_root + 1, #cur_root + 1) ~= '/' then
+          if bufnr == curbuf then
+            pcall(vim.cmd, 'Bdelete! ' .. tostring(bufnr))
+          else
+            pcall(vim.cmd, 'bdelete! ' .. tostring(bufnr))
+          end
+        end
+      end
+    end
+    M.refresh()
+  end
 end
 
 function M.refresh()
