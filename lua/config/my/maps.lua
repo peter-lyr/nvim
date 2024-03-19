def1ab97a2cd3592fc36e46e66cbeee7745d0618
peter-lyr <llydrp.ldp@gmail.com>
2024-03-20 00:23:59 +0800

    fix: MapFromWhichkeyToLazy

--- lua/config/my/maps.lua
+++ lua/config/my/maps.lua
@@ -811,13 +811,13 @@ function M.map_from_lazy_to_whichkey(fname)
   end
   local new_lines = {}
   for _, line in ipairs(vim.fn.readfile(fname)) do
-    local res = string.match(line, '^ +({.*mode *= *.*}),')
+    local res = string.match(line, '^ +({.*mode *= *.*}) *,')
     if res then
       local item = loadstring('return ' .. res)
       if item then
         local val = item()
         if type(val[2]) == 'function' then
-          val[2] = string.match(res, '(function().+end)')
+          val[2] = string.match(res, '(function().+end),')
         end
         local lhs = table.remove(val, 1)
         if not val['name'] then
@@ -825,10 +825,11 @@ function M.map_from_lazy_to_whichkey(fname)
           val['desc'] = nil
         end
         local temp = string.gsub(vim.inspect { [lhs] = val, }, '%s+', ' ')
-        temp = string.gsub(temp, '"(function().+end)"', '%1')
+        temp = string.gsub(temp, '"(function().+end)",', '%1,')
         temp = string.gsub(temp, '\\"', '"')
-        temp = string.gsub(temp, '{(.+)}', '%1') .. ','
-        new_lines[#new_lines + 1] = temp
+        temp = string.gsub(temp, '{(.+)}', '%1')
+        temp = vim.fn.trim(temp) .. ','
+        new_lines[#new_lines + 1] = '  ' .. temp
       else
         new_lines[#new_lines + 1] = line
       end
@@ -852,21 +853,22 @@ function M.map_from_whichkey_to_lazy(fname)
   end
   local new_lines = {}
   for _, line in ipairs(vim.fn.readfile(fname)) do
-    local res = string.match(line, '%[.+%] *= *{.+},')
+    local res = string.match(line, '^.+ *=.*{.*mode *=.+} *,')
     if res then
       local item = loadstring('return {' .. res .. '}')
       if item then
         local val = item()
         for lhs, d in pairs(val) do
           if type(d[1]) == 'function' then
-            d[1] = string.match(res, '(function().+end)')
+            d[1] = string.match(res, '(function().+end),')
           end
           table.insert(d, 1, lhs)
           d['desc'] = table.remove(d, 3)
           local temp = string.gsub(vim.inspect(d), '%s+', ' ')
-          temp = string.gsub(temp, '"(function().+end)"', '%1')
-          temp = string.gsub(temp, '\\"', '"') .. ','
-          new_lines[#new_lines + 1] = temp
+          temp = string.gsub(temp, '"(function().+end)",', '%1,')
+          temp = string.gsub(temp, '\\"', '"')
+          temp = vim.fn.trim(temp) .. ','
+          new_lines[#new_lines + 1] = '  ' .. temp
         end
       else
         new_lines[#new_lines + 1] = line
@@ -881,3 +883,4 @@ end
 vim.opt.updatetime = 500
 
 return M
+
