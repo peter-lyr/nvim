7b2967e7470b9779883421e2ab5b098daaa85e20
peter-lyr <llydrp.ldp@gmail.com>
2024-03-19 23:47:59 +0800

    MapFromWhichkeyToLazy

--- lua/config/my/maps.lua
+++ lua/config/my/maps.lua
@@ -826,8 +826,48 @@ function M.map_from_lazy_to_whichkey(fname)
         end
         local temp = string.gsub(vim.inspect { [lhs] = val, }, '%s+', ' ')
         temp = string.gsub(temp, '"(function().+end)"', '%1')
+        temp = string.gsub(temp, '\\"', '"')
         temp = string.gsub(temp, '{(.+)}', '%1') .. ','
         new_lines[#new_lines + 1] = temp
+      else
+        new_lines[#new_lines + 1] = line
+      end
+    else
+      new_lines[#new_lines + 1] = line
+    end
+  end
+  require 'plenary.path':new(fname):write(vim.fn.join(new_lines, '\r\n'), 'w')
+end
+
+vim.api.nvim_create_user_command('MapFromWhichkeyToLazy', function(params)
+  M.map_from_whichkey_to_lazy(unpack(params['fargs']))
+end, { nargs = 0, })
+
+function M.map_from_whichkey_to_lazy(fname)
+  if not fname then
+    fname = string.gsub(vim.api.nvim_buf_get_name(0), '/', '\\')
+  end
+  if #fname == 0 then
+    return
+  end
+  local new_lines = {}
+  for _, line in ipairs(vim.fn.readfile(fname)) do
+    local res = string.match(line, '%[.+%] *= *{.+},')
+    if res then
+      local item = loadstring('return {' .. res .. '}')
+      if item then
+        local val = item()
+        for lhs, d in pairs(val) do
+          if type(d[1]) == 'function' then
+            d[1] = string.match(res, '(function().+end)')
+          end
+          table.insert(d, 1, lhs)
+          d['desc'] = table.remove(d, 3)
+          local temp = string.gsub(vim.inspect(d), '%s+', ' ')
+          temp = string.gsub(temp, '"(function().+end)"', '%1')
+          temp = string.gsub(temp, '\\"', '"') .. ','
+          new_lines[#new_lines + 1] = temp
+        end
       else
         new_lines[#new_lines + 1] = line
       end
@@ -841,4 +881,3 @@ end
 vim.opt.updatetime = 500
 
 return M
-
