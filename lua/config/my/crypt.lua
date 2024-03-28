f570b72d959fe5c121eb7b39106231b0a96c7103
peter-lyr <llydrp.ldp@gmail.com>
2024-03-28 14:19:01 +0800

    function M.decrypt(ifile, ofile, pass)

--- lua/config/my/crypt.lua
+++ lua/config/my/crypt.lua
@@ -4,8 +4,6 @@
 -- aescrypt.exe -e -p 123456 -o a.txt.bin a.txt
 -- aescrypt.exe -d -p 123456 -o a.txt.dec.txt a.txt.bin
 
--- password: ldp-******
-
 local M = {}
 
 local B = require 'base'
@@ -15,6 +13,47 @@ M.lua = B.getlua(M.source)
 
 M.crypt_exe_dir_path = B.getcreate_dirpath(M.source .. '.exe')
 M.aescrypt_exe = B.get_filepath(M.crypt_exe_dir_path.filename, 'aescrypt.exe').filename
-print("M.aescrypt_exe:", M.aescrypt_exe)
+
+M.encrypt_fts = {
+  'md'
+}
+
+M.decrypt_fts = {
+  'bin'
+}
+
+function M.encrypt(ifile, ofile, pass)
+  if not ifile then
+    ifile = B.buf_get_name_0()
+  end
+  local ext = vim.fn.tolower(string.match(ifile, '%.([^.]+)$'))
+  if not B.is_in_tbl(ext, M.encrypt_fts) then
+    return
+  end
+  if not ofile then
+    ofile = vim.fn.fnamemodify(ifile, ':p:r') .. '.bin'
+  end
+  if not pass then
+    pass = vim.fn.fnamemodify(ifile, ':p:t:r')
+  end
+  B.system_run('start', '%s -e -p %s -o %s %s', M.aescrypt_exe, pass, ofile, ifile)
+end
+
+function M.decrypt(ifile, ofile, pass)
+  if not ifile then
+    ifile = B.buf_get_name_0()
+  end
+  local ext = vim.fn.tolower(string.match(ifile, '%.([^.]+)$'))
+  if not B.is_in_tbl(ext, M.decrypt_fts) then
+    return
+  end
+  if not ofile then
+    ofile = vim.fn.fnamemodify(ifile, ':p:r') .. '.md'
+  end
+  if not pass then
+    pass = vim.fn.fnamemodify(ifile, ':p:t:r')
+  end
+  B.system_run('start', '%s -d -p %s -o %s %s', M.aescrypt_exe, pass, ofile, ifile)
+end
 
 return M
