5327fb55d6924a961ca7de6f5ba15f6c04ff9d11
peter-lyr <llydrp.ldp@gmail.com>
2024-03-28 13:57:56 +0800

    lua/config/my/crypt.lua.exe/aescrypt.exe

---
+++ lua/config/my/crypt.lua
@@ -0,0 +1,15 @@
+-- Copyright (c) 2024 liudepei. All Rights Reserved.
+-- create at 2024/03/28 13:51:07 Thursday
+
+local M = {}
+
+local B = require 'base'
+
+M.source = B.getsource(debug.getinfo(1)['source'])
+M.lua = B.getlua(M.source)
+
+M.crypt_exe_dir_path = B.getcreate_dirpath(M.source .. '.exe')
+M.aescrypt_exe = B.get_filepath(M.crypt_exe_dir_path.filename, 'aescrypt.exe').filename
+print("M.aescrypt_exe:", M.aescrypt_exe)
+
+return M
